bdb01beb2ec8d86a8c8faec7b888655a1eac7a21
Dyako Dyakov <dyakogeorgiev@gmail.com>
2019-06-13 17:20:04 +0300

    add invoker class

---
+++ src/main/java/com/qr/code/generator/QRCodeReaderWriterImpl.java
@@ -0,0 +1,64 @@
+package com.qr.code.generator;
+
+import com.qr.code.generator.commands.QRImage;
+import com.qr.code.generator.commands.ReadQRImageDirectory;
+import com.qr.code.generator.commands.WriteQRImageDirectory;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+
+
+public class QRCodeReaderWriterImpl implements QRCodeReaderWriter{
+
+  private QRImage readQRImageDirectory;
+  private QRImage writeQRImageDirectory;
+
+  public QRCodeReaderWriterImpl(){}
+
+
+  /**
+   * Performs recursive search, and generate the QR Code for every file. Also, move the
+   * generated QR Code inside targetDir, preserving the directory hierarchy.
+   *
+   * @param sourceDir - source directory of the files that will have qr code images
+   * @param targetDir - target directory where the files will be saved preserving the source directory hierarchy
+   *
+   * @throws IOException - if the operation fails
+   * @throws WriterException - if the ZXing library can't create qr code for file
+   */
+
+
+  @Override
+  public void write (File sourceDir, int depth, File targetDir) throws IOException, WriterException, NotFoundException {
+    if(!sourceDir.isDirectory()){
+      throw new NoSuchFileException("Please provide path to existing directory.");
+    }
+    writeQRImageDirectory = new WriteQRImageDirectory(sourceDir, depth, targetDir);
+    writeQRImageDirectory.execute();
+  }
+
+
+  /**
+   * Reads QR code (denoted by sourceDir) and saves the resulted file inside the targetDir, again
+   * preserving the directory hierarchy.
+   *
+   * @param sourceDir - source directory of the qr code files
+   * @param targetDir - target directory where the files will be decoded preserving the source directory hierarchy
+   *
+   * @throws IOException - if the operation fails
+   * @throws NotFoundException - if there isn't any directory with that name
+   */
+
+
+  @Override
+  public void read (File sourceDir, File targetDir) throws IOException, NotFoundException, WriterException {
+    if(!sourceDir.isDirectory()){
+      throw new NoSuchFileException("Please provide path to existing directory.");
+    }
+    readQRImageDirectory = new ReadQRImageDirectory(sourceDir, targetDir);
+    readQRImageDirectory.execute();
+  }
+
+}
